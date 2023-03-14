fda1320335385506b43cd7eae7b766305a186e74
HristijanMitic00 <hristijan.mitic.01@gmail.com>
2023-03-14 17:34:16 +0100

    initial commit

---
+++ Project1/Controllers/RecipesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Recipes
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View(db.recipes.ToList());
+        }
+
+        // GET: Recipes/Details/5
+        [Authorize(Roles ="Administrator,User")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipe recipe = db.recipes.Find(id);
+            if (recipe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipe);
+        }
+
+        // GET: Recipes/Create
+        [Authorize(Roles = "Administrator,User")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recipes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Ingridients,Preparation,RecipeLink,Image,Difficulty")] Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.recipes.Add(recipe);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(recipe);
+        }
+
+        // GET: Recipes/Edit/5
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipe recipe = db.recipes.Find(id);
+            if (recipe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipe);
+        }
+
+        // POST: Recipes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Ingridients,Preparation,RecipeLink,Image,Difficulty")] Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(recipe).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(recipe);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Delete(int id)
+        {
+            Recipe recipe = db.recipes.Find(id);
+            db.recipes.Remove(recipe);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        [Authorize(Roles = "Administrator,User")]
+        public ActionResult RandomRecipe()
+        {
+            int len = (from i in db.recipes select i.Id).Max(); ;
+            Random random = new Random(); 
+            int rand_num = random.Next(1,len+1);
+            Recipe recipe = db.recipes.Find(rand_num);
+            if (recipe == null)
+            {
+                return RandomRecipe();
+            }
+            return View(recipe); 
+        }
+    }
+}
---
+++ Project1/Models/Recipe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project1.Models
+{
+    public class Recipe
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Ingridients { get; set; }
+        [Required]
+        public string Preparation { get; set; }
+        public string RecipeLink { get; set; }
+        public string Image { get; set; }
+        [Range(1, 10, ErrorMessage = "Number between 1 and 10, 1 - easiest, 10 - hardest")]
+        [Required]
+        public int Difficulty { get; set; }
+    }
+}
